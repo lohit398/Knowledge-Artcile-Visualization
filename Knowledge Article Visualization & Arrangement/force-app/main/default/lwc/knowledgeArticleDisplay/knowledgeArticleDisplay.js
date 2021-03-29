cca10f033a1a3fb1994d40339892fe06bcf8509f
Lohit <lohitkrishna39@gmail.com>
2021-03-30 01:09:10 +0530

    knowledge article category tree updated

--- Knowledge Article Visualization & Arrangement/force-app/main/default/lwc/knowledgeArticleDisplay/knowledgeArticleDisplay.js
+++ Knowledge Article Visualization & Arrangement/force-app/main/default/lwc/knowledgeArticleDisplay/knowledgeArticleDisplay.js
@@ -3,82 +3,16 @@ import getKnowledgeCategories from '@salesforce/apex/DE_KnowledgeVisualizationHe
 
 
 export default class KnowledgeArticleDisplay extends LightningElement {
-    @track knowledgeCategories = [];
     selectedKnowledgeArticleId;
     @api recordId;
 
-    /*wire Method to fetch categories and organise appropriately*/
-    @wire(getKnowledgeCategories)
-    getCategories({ error, data }) {
-        if (error)
-            console.log(error);
-        else if (data) {
-            JSON.parse(JSON.stringify(data)).map(item => {
-                let found = 0;
-
-                for (let i = 0; i < this.knowledgeCategories.length; i++) {
-                    if (item.DataCategoryGroupName === this.knowledgeCategories[i].DataCategoryGroupName) {
-                        found = 1;
-                        for (let [index, article] of this.knowledgeCategories[i].articleDetails.entries()) {
-                            if (article.DataCategoryName === item.DataCategoryName) {
-                                let obj = {};
-                                obj.articleId = item.ParentId;
-                                obj.articleName = item.Parent.Title;
-                                this.knowledgeCategories[i].articleDetails[index].articles.push(obj);
-
-                            }
-                            let currentCategories = this.knowledgeCategories[i].articleDetails.map(dataCategory => {
-                                return dataCategory.DataCategoryName;
-                            });
-                            if (currentCategories.indexOf(item.DataCategoryName) == -1) {
-                                let obj = {};
-                                obj.DataCategoryName = item.DataCategoryName;
-                                obj.articles = [];
-                                /*let subObj = {};
-                                subObj.articleName = item.Parent.Title;
-                                subObj.articleId = item.ParentId;
-                                obj.articles.push(subObj);*/
-                                this.knowledgeCategories[i].articleDetails.push(obj);
-                            }
-                        }
-                    }
-                }
-
-                if (found === 0) {
-                    let new_obj = {
-                        DataCategoryGroupName: item.DataCategoryGroupName,
-                        articleDetails: [],
-                    };
-                    let articleDetail = {
-                        DataCategoryName: item.DataCategoryName,
-                        articles: [],
-                    };
-                    let subObj = {};
-                    subObj.articleName = item.Parent.Title;
-                    subObj.articleId = item.ParentId;
-                    articleDetail.articles.push(subObj);
-                    
-                    new_obj.articleDetails.push(articleDetail);
-
-                    this.knowledgeCategories.push(new_obj);
-                }
-            })
-        }
-        this.selectedKnowledgeArticleId = this.recordId;
+    connectedCallback(){
+        if(this.recordId != undefined || this.recordId != null)
+            this.selectedKnowledgeArticleId = this.recordId;
     }
 
-
-
-    // handlers
-    handleArticleClick(event) {
-        this.selectedKnowledgeArticleId = event.currentTarget.dataset.knowledgeid;
-        this.template.querySelector('.active-border') != null || this.template.querySelector('.active-border') != undefined ? this.template.querySelector('.active-border').classList.remove('active-border') : "First Time";
-
-        this.template.querySelectorAll('[data-knowledgeid="' + this.selectedKnowledgeArticleId + '"]').forEach(element => {
-            if (element.dataset.category === event.currentTarget.dataset.category) {
-                element.classList.add('active-border');
-            }
-        });
-        console.log(this.knowledgeCategories);
+    handleArticleSelected(event){
+        this.selectedKnowledgeArticleId = event.detail;
+        console.log(this.selectedKnowledgeArticleId);
     }
 }
---
+++ Knowledge Article Visualization & Arrangement/force-app/main/default/lwc/knowledgeArticleTreeVisualizer/knowledgeArticleTreeVisualizer.js
@@ -0,0 +1,90 @@
+import { LightningElement, wire, track, api } from 'lwc';
+import getCategoryStructures from '@salesforce/apex/KnowledgeTreeVisualizer_Helper.getCategoryStructures';
+import getKnowledgeArticles from '@salesforce/apex/KnowledgeTreeVisualizer_Helper.getKnowledgeArticles';
+
+export default class KnowledgeArticleTreeVisualizer extends LightningElement {
+
+    @track categories = []; // Tree data (Categories) goes into this variable
+    @api recordId; //input variable to get clicked knowledge article Id
+
+    /*This is the wire method to get data category tree*/
+    @wire(getCategoryStructures)
+    getAllCategoryStructures({ error, data }) {
+        if (error)
+            console.log(error);
+        else if (data) {
+            /*Parsed the content to make it available in tree structure */
+            this.categories = JSON.parse(data).map((item) => {
+                let group = {};
+                group.label = item.label;
+                group.name = item.label;
+                group.items = item.topCategories[0].childCategories.map(category => {
+                    return category;
+                });
+                return group;
+            })
+            this.categories = JSON.parse(JSON.stringify(this.categories).replaceAll('childCategories', 'items').replaceAll('"items":[]', '"items":[{"label":"No Articles","name":"No Articles","items":[]}]'));
+            console.log(JSON.stringify(this.categories));
+            this.getKnowledgeArticles();
+        }
+    }
+
+    getKnowledgeArticles() {
+        getKnowledgeArticles()
+            .then(data => {
+                JSON.parse(JSON.stringify(data)).map(item => {
+                    let articleDataCategoryGroup = item.DataCategoryGroupName;
+                    let articleDataCategoryName = item.DataCategoryName;
+                    let article = item;
+                    this.insertArticles(articleDataCategoryName, article, articleDataCategoryGroup);
+                })
+                //console.log(data);
+            }).catch(error => {
+                console.log(error);
+            })
+    }
+
+    /*Traverse through objects*/
+    insertArticles(articleDataCategoryName, article, articleDataCategoryGroup) {
+        let categoryTree = this.categories;
+        for (let i = 0; i < categoryTree.length; i++) {
+            if (categoryTree[i].label === articleDataCategoryGroup) {
+                this.traverseUpdateTree(categoryTree[i].items, article, articleDataCategoryName);
+            }
+        }
+        //console.log(JSON.stringify(categoryTree));
+        this.categories = JSON.parse(JSON.stringify(categoryTree));
+    }
+
+    /*TRAVERSE through nested objects and update the tree*/
+    traverseUpdateTree(category, article, articleDataCategoryName) {
+        category.map(item => {
+            if (item.label === articleDataCategoryName) {
+                //console.log(JSON.stringify(item));
+                if (item.items.length === 1) {
+                    item.items.pop();
+                }
+                let obj = {};
+                if (article.Parent.Summary != null && article.Parent.Summary != undefined) {
+                    obj.label = article.Parent.Summary;
+                }
+                else {
+                    obj.label = article.Parent.Title;
+                }
+                obj.name = article.Parent.Id;
+                obj.items = [];
+                item.items.push(obj);
+            } else if (item.items.length != 0) {
+                this.traverseUpdateTree(item.items);
+            }
+        })
+    }
+
+    /*Getting the selecting article and making it available to the customer*/
+    handleArticleSelect(event) {
+        if (event.detail.name != "No Articles") {
+            const evt = new CustomEvent('articleselected', { detail: event.detail.name });
+            this.dispatchEvent(evt);
+        }
+    }
+}
